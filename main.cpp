f18b772d07cf975df0c68167a2583fa04ebe11fd
Eugeniy Shablinskiy <zcoder@mail.ru>
2011-11-24 23:33:09 +0400

    nop: in the begin of work

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: zhenka
+ *
+ * Created on 24 Ноябрь 2011 г., 21:57
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
